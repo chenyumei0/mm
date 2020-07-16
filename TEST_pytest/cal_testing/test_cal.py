e5aa8351c54a380ca29540711b7f06cded72b222
18700460110 <18700460110@163.com>
2020-07-16 16:50:41 +0800

    pytest第二次作业提交 Signed-off-by: 18700460110 <18700460110@163.com>

--- TEST_pytest/cal_testing/test_cal.py
+++ TEST_pytest/cal_testing/test_cal.py
@@ -4,15 +4,45 @@ import yaml
 
 from cal_code.cal import Calulator
 
+def test_env(openenv):
+    env,datas=openenv
+    print(f"环境：{env},数据:{datas}")
+    ip = datas['env']['ip']
+    port =datas['env']['port']
+    url ='http://'+ip+':'+port
+    print(url)
+
+
+#获取测试数据
+with open('cal_testing/testing_data/data.yml',encoding='utf-8') as f:
+    datas=yaml.safe_load(f)
+
+    myids_div=datas['div'].keys()
+    mydatas_div=datas['div'].values()
+
+    myids_add=datas['add'].keys()
+    mydatas_add=datas['add'].values()
+
+    myids_mutl=datas['mutl'].keys()
+    mydatas_mutl=datas['mutl'].values()
+
+    myids_subt=datas['subt'].keys()
+    mydatas_subt=datas['subt'].values()
+
+#使用fixture初始化用例
 @pytest.fixture()
 def method():
     print("set_up,开始计算")
     yield
     print("teat_down,计算结束")
+
+#定义测试类
 class TestCal:
 
-    @pytest.mark.parametrize("a,b",yaml.safe_load(open("testing_data/data.yml",encoding='utf-8')),
-                             ids=yaml.safe_load(open("testing_data/Casetitle_add.yml",encoding='utf-8')))
+#加法测试
+    @pytest.mark.run(order=1)
+    @pytest.mark.dependency(name='test_add')
+    @pytest.mark.parametrize("a,b",mydatas_add,ids=myids_add)
     def test_add(self,cal_begin,method,a,b):
         print("测试加法")
         cal =Calulator()
@@ -21,8 +51,10 @@ class TestCal:
         except:return cal.add(a, b)
         print(f'测试结果为{cal.add(a,b)}')
 
-    @pytest.mark.parametrize("a,b", yaml.safe_load(open("testing_data/data_div.yml",encoding='utf-8')),
-                             ids=yaml.safe_load(open("testing_data/Casetitle_subt.yml",encoding='utf-8')))
+#减法测试
+    @pytest.mark.run(order=2)
+    @pytest.mark.dependency(depends=['test_add'],name='test_subt')
+    @pytest.mark.parametrize("a,b", mydatas_subt,ids=myids_subt)
     def test_subt(self,cal_begin,method,a,b):
         print("测试减法")
         cal = Calulator()
@@ -30,19 +62,23 @@ class TestCal:
             assert cal.subt(a, b) == a - b
         except:return cal.subt(a, b)
         print(f'测试结果为{cal.subt(a, b)}')
-
-    @pytest.mark.parametrize("a,b", yaml.safe_load(open("testing_data/data.yml",encoding='utf-8')),
-                        ids=yaml.safe_load(open("testing_data/Casetitle_mutl.yml",encoding='utf-8')))
-    def test_mutl(self,cal_begin,method, a, b):
+#乘法测试
+    @pytest.mark.run(order=3)
+    @pytest.mark.dependency(name='test_mutl')
+    @pytest.mark.parametrize("a,b", mydatas_mutl,ids=myids_mutl)
+    def check_mutl(self,cal_begin,method, a, b):
         print("测试乘法")
         cal = Calulator()
         try:
             assert cal.multi(a, b) == a * b
+            assert a==b
         except:return cal.multi(a, b)
         print(f'测试结果为{cal.multi(a, b)}')
 
-    @pytest.mark.parametrize("a,b", yaml.safe_load(open("testing_data/data_div.yml",encoding='utf-8')),
-                                    ids=yaml.safe_load(open("testing_data/Casetitle_div.yml",encoding='utf-8')))
+#除法测试
+    @pytest.mark.run(order=4)
+    @pytest.mark.dependency(depends=["test_mutl"],name="test_div")
+    @pytest.mark.parametrize("a,b", mydatas_div,ids=myids_div)
     def test_div(self,cal_begin,method,a,b):
         print("测试除法")
         cal = Calulator()
---
+++ TEST_pytest/conftest.py
@@ -0,0 +1,58 @@
+﻿import pytest
+import yaml
+
+
+def pytest_collection_modifyitems(items):
+    """
+    测试用例收集完成时，将收集到的item的name和nodeid的中文显示在控制台上
+    :return:
+    """
+    for item in items:
+        item.name = item.name.encode("utf-8").decode("unicode_escape")
+        item._nodeid = item.nodeid.encode("utf-8").decode("unicode_escape")
+#为测试用例添加Mark标签
+    #
+    # for item in items:
+    #     if 'add' in item.nodeid:
+    #         item.add.marker(pytest.mark.add)
+    #     if 'mutl' in item.nodeid:
+    #         item.add.marker(pytest.mark.mutl)
+    #     if 'subt' in item.nodeid:
+    #         item.add.marker(pytest.mark.subt)
+    #     if 'div' in item.nodeid:
+    #         item.add.marker(pytest.mark.div)
+
+
+#用例公共初始化部分
+@pytest.fixture(scope="session")
+def cal_begin():
+    print("setup,初始化")
+    yield
+
+    print("teardown，环境恢复")
+
+
+def pytest_addoption(parser):
+    mygroup=parser.getgroup('hogwarts')
+    mygroup.addoption('--env',
+                      default = 'test',
+                      dest='env',
+                      help='set your env')
+
+
+@pytest.fixture(scope='session')
+def openenv(request):
+    myenv=request.config.getoption('--env',default='test')
+    if myenv =='test':
+        datapath = 'cal_testing/testing_data/data_test.yml'
+
+    if myenv =='dev':
+        datapath = 'cal_testing/testing_data/data_dev.yml'
+
+    if myenv == 'st':
+        datapath = 'cal_testing/testing_data/data_st.yml'
+
+    with open(datapath) as f:
+        datas = yaml.safe_load(f)
+
+    return myenv,datas
